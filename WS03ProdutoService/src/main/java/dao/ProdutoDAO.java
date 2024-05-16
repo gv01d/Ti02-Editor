04d9c1bb6a458fc85c0024b6ddff92cb6b1800d0
Iago Fereguetti <130393481+Iago-Fereguetti18@users.noreply.github.com>
2024-05-15 22:53:14 -0300

    Add files via upload

---
+++ WS03ProdutoService/src/main/java/dao/ProdutoDAO.java
@@ -0,0 +1,146 @@
+package dao;
+
+import model.Produto;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ProdutoDAO extends DAO {	
+	public ProdutoDAO() {
+		super();
+		conectar();
+	}
+	
+	
+	public void finalize() {
+		close();
+	}
+	
+	
+	public boolean insert(Produto produto) {
+		boolean status = false;
+		try {
+			String sql = "INSERT INTO produto (nomecompleto, email, datanascimento, instrumento, descricao) "
+		               + "VALUES ('" + produto.getNomeCompleto() + "', "
+		               + produto.getEmail() + ", ?,"
+                       + ", " + produto.getInstrumento()
+                       + ", " + produto.getDescricao();
+			PreparedStatement st = conexao.prepareStatement(sql);
+			st.setDate(1, Date.valueOf(produto.getDataNascimento()));
+			st.executeUpdate();
+			st.close();
+			status = true;
+		} catch (SQLException u) {  
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+
+	
+	public Produto get(int id) {
+        Produto produto = null;
+        
+        try {
+            Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            String sql = "SELECT * FROM produto WHERE id=" + id;
+            ResultSet rs = st.executeQuery(sql);
+            if (rs.next()) {
+                produto = new Produto(
+                		rs.getInt("id"),
+                        rs.getString("nomecompleto"),
+                        rs.getString("email"),
+                        rs.getDate("datanascimento").toLocalDate(),
+                        rs.getString("descricao"),
+                        rs.getString("instrumento")
+                );
+            }
+            st.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return produto;
+    }
+	
+	
+	public List<Produto> get() {
+		return get("");
+	}
+
+	
+	public List<Produto> getOrderByID() {
+		return get("id");		
+	}
+	
+	
+	public List<Produto> getOrderByDescricao() {
+		return get("descricao");		
+	}
+	
+	
+	public List<Produto> getOrderByPreco() {
+		return get("preco");		
+	}
+	
+	
+	private List<Produto> get(String orderBy) {
+		List<Produto> produtos = new ArrayList<Produto>();
+		
+		try {
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			String sql = "SELECT * FROM produto" + ((orderBy.trim().length() == 0) ? "" : (" ORDER BY " + orderBy));
+			ResultSet rs = st.executeQuery(sql);	           
+	        while(rs.next()) {	            	
+	        	Produto p = new Produto(rs.getInt("id"), rs.getString("nomecompleto"), rs.getString("email"),
+	        			rs.getDate("dataNascimento").toLocalDate(),
+	        			rs.getString("instrumento"), rs.getString("descricao"));
+	            produtos.add(p);
+	        }
+	        st.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return produtos;
+	}
+	
+	
+	public boolean update(Produto produto) {
+		boolean status = false;
+		try {  
+			String sql = "UPDATE produto SET nomecompleto = '" + produto.getNomeCompleto() + "', "
+					   + "email = " + produto.getEmail() + ", " 
+					   + "datanascimento = ?, " 
+					   + "instrumento = " + produto.getInstrumento() + ", " 
+					   + "descricao = " + produto.getDescricao()
+					   + " WHERE id = " + produto.getId();
+			PreparedStatement st = conexao.prepareStatement(sql);
+			st.setDate(1, Date.valueOf(produto.getDataNascimento()));
+			st.executeUpdate();
+			st.close();
+			status = true;
+		} catch (SQLException u) {  
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+	
+	
+	public boolean delete(int id) {
+		boolean status = false;
+		try {  
+			Statement st = conexao.createStatement();
+			st.executeUpdate("DELETE FROM produto WHERE id = " + id);
+			st.close();
+			status = true;
+		} catch (SQLException u) {  
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+}
---
+++ WS03ProdutoService/src/main/java/model/Produto.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Produto {
+	private int id;
+    private String nomeCompleto;
+    private String email;
+    private LocalDate dataNascimento;
+    private String instrumento;
+    private String descricao;
+    
+    // Construtores
+    public Produto() {
+    	id = 0;
+        nomeCompleto = "";
+        email = "";
+        dataNascimento = LocalDate.now();
+        instrumento = "";
+        descricao = "";
+    }
+
+    public Produto(int id, String nomeCompleto, String email, LocalDate dataNascimento, String instrumento, String descricao) {
+    	this.id = id;
+        this.nomeCompleto = nomeCompleto;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.instrumento = instrumento;
+        this.descricao = descricao;
+    }
+
+    // Métodos de acesso
+    public int getId() {
+    	return id;
+    }
+    
+    public void setId(int id) {
+    	this.id = id;
+    }
+    
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getInstrumento() {
+        return instrumento;
+    }
+
+    public void setInstrumento(String instrumento) {
+        this.instrumento = instrumento;
+    }
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
